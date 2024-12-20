f7557685447590a9e1068fc5328584e9a7ba0200
ashwi <ashwi@AshwiniPavan>
2024-12-20 21:08:03 +0530

    Rest Assured Project

---
+++ gitPractice/src/test/java/gitPractice/TestGitClass.java
@@ -0,0 +1,14 @@
+package gitPractice;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.Test;
+
+public class TestGitClass {
+
+	@Test
+	void rest() {
+		given().when().get("https://reqres.in/api/users?page=2").then().statusCode(200);
+	}
+
+}
